680aed6d6d6a281a46c9f9fbe929b9e0b46e7464
Pushok8 <60236064+Pushok8@users.noreply.github.com>
2020-04-10 18:00:23 +0300

    Update Most popular symbol in text.py

--- Most popular symbol in text.py
+++ Most popular symbol in text.py
@@ -1,27 +1,40 @@
-def getKeyByValue(dic, val, alphabet):
-    for letter in alphabet:
+# Creator: ©Pushok8.
+# Date: 25 March 2020.
+# Edit 10 April 2020.
+# Added documentation and other characters became available.
+
+CHARS = []
+for i in range(12679 + 1):
+    CHARS.append(chr(i))
+
+
+def get_key_by_value(dic, val, CHARS):
+    for letter in CHARS:
         if dic[letter] == val:
             return letter
 
 
-def mostPopularSymbolInText(text):
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z', ' ', '!', '.', ',', '?', '_', '-', '+', '=', '>', '<', '/', '\\', '@', '#',
-                '0',
-                '1', '2', '3', '4', '5', '6', '7', '8', '9', '"', '\'', '|', ')', '(', ':', ';', '`', '~', '*', '&',
-                '^', '%', '$', '№']
+def top_symb(text):
+    '''
+    top_sumb(text)
+
+    Searches for the most frequently occurring character in the text.
+    If two characters occur equally often,then the most recent one is returned.
+    :param text: The text in which you need to find a frequently occurring character.
+    :return:     Returns the character and the number of references to it in the text.
+    '''
 
     for i in text:
-        if not i in alphabet:
-            return 'Your text is incorrect!'
+        assert i in CHARS, 'There is no such symbol in the list.'
     if text == '':
         return 'Your text is incorrect!'
 
-    dic = {}
-    for letter in alphabet:
+    quantity_letters = {}
+
+    for letter in CHARS:
         sumSymbolInText = text.count(letter)
-        dic.update({letter: sumSymbolInText})
-    maxVal = max(dic.values())
-    maxKey = getKeyByValue(dic, maxVal, alphabet)
-    topSymbol = 'Most popular symbol in this text - ' + str(maxKey) + '. His value - ' + str(maxVal)
-    return topSymbol
+        quantity_letters.update({letter: sumSymbolInText})
+
+    maxVal = max(quantity_letters.values())
+    maxKey = get_key_by_value(quantity_letters, maxVal, CHARS)
+    return (maxKey, maxVal)
